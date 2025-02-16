6450ee9e5de2da34385c1be8aa419cd128ed11ef
Mark A. <mark001616@gmail.com>
2025-02-16 12:23:47 -0500

    Add basic routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "./App.jsx";
+import Login from "./pages/Login.jsx";
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
-)
+);
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,6 +2,9 @@ export default function Login() {
   return (
     <>
       <h1>Log In</h1>
+      <a href="http://localhost:3000/api/auth/google" className="button">
+        Sign in with Google
+      </a>
     </>
   );
 }
