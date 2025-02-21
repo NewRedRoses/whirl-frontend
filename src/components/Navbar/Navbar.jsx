0349e4c21421b06c534864353a653b46ed34e8ef
Mark A. <mark001616@gmail.com>
2025-02-21 02:26:46 -0500

    Add profile picture to navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,8 @@
 import { Link } from "react-router";
 import styles from "./navbar.module.css";
+import { useUser } from "../../main.jsx";
 export default function Navbar() {
+  const { user, setUser } = useUser();
   return (
     <>
       <div className={styles.navbar}>
@@ -9,7 +11,11 @@ export default function Navbar() {
         </div>
 
         <div className={styles.right}>
-          <Link to="/profile">(pfp_goes here)</Link>
+          <Link to="/profile">
+            {user && (
+              <img src={user.pfpUrl} alt="" className={styles["navbar-pfp"]} />
+            )}
+          </Link>
         </div>
       </div>
     </>
