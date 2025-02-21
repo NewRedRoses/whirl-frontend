dbe9665d6dc957c72a23a4504f2360fe683260b6
Mark A. <mark001616@gmail.com>
2025-02-21 02:21:22 -0500

    Update context user data post login

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,33 @@
 import { useEffect, useState } from "react";
+import { UserContext, useUser } from "./main.jsx";
 import Post from "./components/Post/Post";
 import "./App.css";
+
 function App() {
+  const { user, setUser } = useUser();
   const [posts, setPosts] = useState([]);
 
-  const url = `${import.meta.env.VITE_BACKEND_URL}/posts`;
+  const userUrl = `${import.meta.env.VITE_BACKEND_URL}/user`;
+  const postsUrl = `${import.meta.env.VITE_BACKEND_URL}/posts`;
 
   useEffect(() => {
-    fetch(url, {
+    // Fetch user
+    fetch(userUrl, {
+      credentials: "include",
+    })
+      .then((response) => response.json())
+      .then((data) => setUser(data));
+
+    // Fetch posts
+    fetch(postsUrl, {
       credentials: "include",
     })
       .then((response) => response.json())
       .then((data) => setPosts(data));
-  }, []);
+  }, [userUrl, postsUrl]);
 
   return (
-    <>
+    <UserContext.Provider value={user}>
       <ul className={"posts-list"}>
         {posts.map((post) => {
           return (
@@ -33,7 +45,7 @@ function App() {
           );
         })}
       </ul>
-    </>
+    </UserContext.Provider>
   );
 }
 
