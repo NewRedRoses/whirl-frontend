972ea98fa86485261844730a5853a817c8b06d50
Mark A. <mark001616@gmail.com>
2025-02-23 14:58:52 -0500

    Add sidebar component for page nav

---
+++ src/components/SideBar/Sidebar.jsx
@@ -0,0 +1,34 @@
+import { Link, useLocation } from "react-router";
+import { Heart, Users, Newspaper } from "lucide-react";
+
+import styles from "./sidebar.module.css";
+
+export default function Sidebar() {
+  const location = useLocation();
+
+  const list = [
+    { name: "Posts", url: "/", icon: <Heart /> },
+    { name: "Users", url: "/users", icon: <Users /> },
+  ];
+  return (
+    <>
+      <ul className={styles.links}>
+        {list.map((link, index) => (
+          <li key={index} className={styles["list-item"]}>
+            <Link
+              to={link.url}
+              className={
+                location.pathname == link.url
+                  ? `${styles.link} ${styles.active}`
+                  : `${styles.link}`
+              }
+            >
+              {link.icon}
+              {link.name}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
