47ce00787510637c4679e539fcb3cb2af43ab7b4
Mark A. <mark001616@gmail.com>
2025-02-24 23:15:29 -0500

    Independentize navbar & remove cached img

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,23 +1,45 @@
 import { Link } from "react-router";
+import { House, KeyRound } from "lucide-react";
+import { useState, useEffect } from "react";
+
 import styles from "./navbar.module.css";
+
 export default function Navbar() {
-  const pfpUrl = localStorage.getItem("pfpUrl");
+  const [pfp, setPfp] = useState(null);
+
+  const userUrl = `${import.meta.env.VITE_BACKEND_URL}/user/pfp`;
+
+  useEffect(() => {
+    fetch(userUrl, {
+      credentials: "include",
+    }).then(async (response) => {
+      if (response.status == 200) {
+        const data = await response.json();
+        setPfp(data.pfpUrl);
+      }
+    });
+  }, [userUrl]);
 
   return (
     <>
-      <div className={styles.navbar}>
-        <div className={styles.left}>
-          <Link to="/">Home</Link>
-        </div>
+      {" "}
+      {pfp && (
+        <div className={styles.navbar}>
+          <div className={styles.left}>
+            <Link to="/">
+              <House />
+            </Link>
+          </div>
 
-        <div className={styles.right}>
-          <Link to="/profile">
-            {pfpUrl && (
-              <img src={pfpUrl} alt="" className={styles["navbar-pfp"]} />
-            )}
-          </Link>
+          {pfp && (
+            <div className={styles.right}>
+              <Link to="/profile">
+                <img src={pfp} alt="" className={styles["navbar-pfp"]} />
+              </Link>
+            </div>
+          )}
         </div>
-      </div>
+      )}
     </>
   );
 }
