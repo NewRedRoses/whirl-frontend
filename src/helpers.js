cf0cfe374b947dc8b8740cfda7e2377cac143398
Mark A. <mark001616@gmail.com>
2025-02-25 20:11:41 -0500

    Add GET & POST helper functions for validated requests

---
+++ src/helpers.js
@@ -0,0 +1,18 @@
+const validatedGetReq = async (url) => {
+  return await fetch(url, {
+    credentials: "include",
+  });
+};
+
+const validatedPostReq = async (url, content) => {
+  return await fetch(url, {
+    method: "POST",
+    credentials: "include",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    body: new URLSearchParams({ content }),
+  });
+};
+
+export { validatedGetReq, validatedPostReq };
