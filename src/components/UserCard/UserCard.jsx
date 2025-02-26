23738db5d8d8fb70c612b7cfe267c5e2c54112ab
Mark A. <mark001616@gmail.com>
2025-02-26 00:08:45 -0500

    Display user cards for all users registered

---
+++ src/components/UserCard/UserCard.jsx
@@ -0,0 +1,33 @@
+import styles from "./usercard.module.css";
+import { UserPlus } from "lucide-react";
+export default function UserCard({
+  username = "",
+  displayName,
+  pfpUrl,
+  friendsCount,
+}) {
+  return (
+    <div className={styles["user-card-container"]}>
+      <div className={styles["user-card-left"]}>
+        <img src={pfpUrl} alt="" className={styles["user-pfp"]} />
+        <div className={styles["user-card-mid"]}>
+          <div className={styles["top-details"]}>
+            <strong>{displayName}</strong>
+          </div>
+          <div className={styles["bottom-details"]}>
+            <span>@{username}</span>
+            <span>{friendsCount} friends</span>
+          </div>
+        </div>
+      </div>
+      <div className={styles["user-card-right"]}>
+        <button
+          className={styles["add-btn"]}
+          onClick={() => console.log("test")}
+        >
+          <UserPlus />
+        </button>
+      </div>
+    </div>
+  );
+}
--- src/pages/Users/Users.jsx
+++ src/pages/Users/Users.jsx
@@ -1,13 +1,36 @@
+import { useState, useEffect } from "react";
 import Sidebar from "../../components/SideBar/Sidebar.jsx";
 import useCheckSession from "../../hooks/useCheckSession.jsx";
+import { validatedGetReq } from "../../helpers.js";
+import UserCard from "../../components/UserCard/UserCard.jsx";
 
 export default function Users() {
+  const [users, setUsers] = useState([]);
   useCheckSession();
+
+  const userUrl = `${import.meta.env.VITE_BACKEND_URL}/user`;
+
+  useEffect(() => {
+    validatedGetReq(userUrl)
+      .then((response) => response.json())
+      .then((data) => setUsers(data));
+  }, [userUrl]);
   return (
     <div className="content">
       <Sidebar />
       <div className="main-content">
         <h1>All Users</h1>
+        {users.map((user, index) => {
+          return (
+            <UserCard
+              key={index}
+              username={user.user.username}
+              displayName={user.displayName}
+              pfpUrl={user.pfpUrl}
+              friendsCount={user.friendsCount}
+            />
+          );
+        })}
       </div>
     </div>
   );
