39848870308e0f39bd6cfbe6ed5fc5611b438cd2
Mark A. <mark001616@gmail.com>
2025-02-25 22:54:24 -0500

    Move styling logic to be handled in css module

--- src/components/Posts/Posts.jsx
+++ src/components/Posts/Posts.jsx
@@ -1,11 +1,13 @@
 import { formatDistance } from "date-fns";
 import Post from "../Post/Post";
 
+import styles from "./posts.module.css";
+
 export default function Posts({ posts }) {
   const today = new Date();
 
   return (
-    <ul className={"posts-list"}>
+    <ul className={styles["posts-list"]}>
       {posts.map((post) => {
         return (
           <li key={post.id}>
