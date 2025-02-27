147934cd5c18f8abf78d535d3831d3afbf95924d
Mark A. <mark001616@gmail.com>
2025-02-26 23:52:21 -0500

    Make a list of users

--- src/pages/Users/Users.jsx
+++ src/pages/Users/Users.jsx
@@ -4,6 +4,8 @@ import useCheckSession from "../../hooks/useCheckSession.jsx";
 import { validatedGetReq } from "../../helpers.js";
 import UserCard from "../../components/UserCard/UserCard.jsx";
 
+import styles from "./users.module.css";
+
 export default function Users() {
   const [users, setUsers] = useState([]);
   useCheckSession();
@@ -20,17 +22,20 @@ export default function Users() {
       <Sidebar />
       <div className="main-content">
         <h1>All Users</h1>
-        {users.map((user, index) => {
-          return (
-            <UserCard
-              key={index}
-              username={user.user.username}
-              displayName={user.displayName}
-              pfpUrl={user.pfpUrl}
-              friendsCount={user.friendsCount}
-            />
-          );
-        })}
+        <ul className={styles.list}>
+          {users.map((user, index) => {
+            return (
+              <li key={index}>
+                <UserCard
+                  username={user.user.username}
+                  displayName={user.displayName}
+                  pfpUrl={user.pfpUrl}
+                  friendsCount={user.friendsCount}
+                />
+              </li>
+            );
+          })}
+        </ul>
       </div>
     </div>
   );
