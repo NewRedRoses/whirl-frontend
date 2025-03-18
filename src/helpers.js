977b4a5f04a35eec98ddef22082cefd113f1c215
Mark A. <mark001616@gmail.com>
2025-03-18 14:59:14 -0400

    Add CORS mode to the requests & address vercel file
    If this actually fixes it that would be superb.

--- src/helpers.js
+++ src/helpers.js
@@ -1,6 +1,7 @@
 const validatedGetReq = async (url) => {
   return await fetch(url, {
     credentials: "include",
+    mode: "cors",
   });
 };
 
@@ -8,6 +9,7 @@ const validatedPostReq = async (url, content = "") => {
   return await fetch(url, {
     method: "POST",
     credentials: "include",
+    mode: "cors",
     headers: {
       "Content-Type": "application/x-www-form-urlencoded",
     },
